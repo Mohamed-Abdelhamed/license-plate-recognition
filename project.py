9049d256d937cc15f5a99d789baf74d9b9a11f5a
Mark <mark.refaat.ramzy@gmail.com>
2020-05-16 08:08:28 +0200

    add comments

--- project.py
+++ project.py
@@ -8,10 +8,13 @@ gray = cv2.imread(img_path, 0)
 cv2.imshow("img", image)
 height, width = gray.shape
 
-withoutnoise = cv2.medianBlur(gray, 5)
-_, bw = cv2.threshold(withoutnoise, 150, 255, cv2.THRESH_BINARY)
+withoutnoise = cv2.medianBlur(gray, 5) # 5 => size of the kernal
+_, bw = cv2.threshold(withoutnoise, 150, 255, cv2.THRESH_BINARY) #any pixel above 150 will be 255
 cv2.imshow("Binary", bw)
 
+# If a pixel gradient is higher than the upper threshold, the pixel is accepted as an edge
+# If a pixel gradient value is below the lower threshold, then it is rejected.
+# If the pixel gradient is between the two thresholds, then it will be accepted only if it is connected to a pixel that is above the upper threshold.
 edges = cv2.Canny(bw, 0, 255)
 cv2.imshow("Edges", edges)
 
@@ -19,11 +22,18 @@ kernel = np.ones((3, 3), np.uint8)
 closing = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
 cv2.imshow("Closing", closing)
 
+# cv2.RETR_LIST retrieves all of the contours without establishing any hierarchical relationships.
+# cv2.CHAIN_APPROX_SIMPLE compresses horizontal, vertical, and diagonal segments and leaves only their end points.
 contours, _ = cv2.findContours(closing, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 allContoursImage = image.copy()
+
+# -1 => contourIdx – Parameter indicating a contour to draw. If it is negative, all the contours are drawn.
+# (0, 255, 0) => green color
+# 3 => thickness
 cv2.drawContours(allContoursImage, contours, -1, (0, 255, 0), 3)
 cv2.imshow("All Contours", allContoursImage)
 
+# sort contours with respect to area descending get first 30 only
 cnts = sorted(contours, key=cv2.contourArea, reverse=True)[:30]
 NumberPlateCnt = None
 top30ContoursImage = image.copy()
@@ -35,7 +45,7 @@ ROI = image.copy()
 for contour in cnts:
     area = cv2.contourArea(contour)
     x, y, w, h = cv2.boundingRect(contour)
-    plate = cv2.approxPolyDP(contour, 0.02 * cv2.arcLength(contour, True), True)
+    # plate = cv2.approxPolyDP(contour, 0.02 * cv2.arcLength(contour, True), True)
     center_y = y + h / 2
     if area > 700 and (w > h) and center_y > height / 2:
         ROI = image[y:y + h, x:x + w]
